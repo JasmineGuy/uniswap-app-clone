f5181a248733163e08f773a0c7ba77c5e1ba2c3d
JasmineGuy <guyjasmine.c@gmail.com>
2021-09-30 20:53:36 -0400

    step 5 need to add modal

--- src/components/Identicon.tsx
+++ src/components/Identicon.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { useEthers } from "@usedapp/core";
-import styled from "@emotion/styled";
 import Jazzicon from "@metamask/jazzicon";
+import styled from "@emotion/styled";
 
 const StyledIdenticon = styled.div`
   height: 1rem;
@@ -17,17 +17,9 @@ export default function Identicon() {
   useEffect(() => {
     if (account && ref.current) {
       ref.current.innerHTML = "";
+      ref.current.appendChild(Jazzicon(16, parseInt(account.slice(2, 10), 16)));
     }
   }, [account]);
 
   return <StyledIdenticon ref={ref as any} />;
 }
-
-// useEffect(() => {
-//   if (account && ref.current) {
-//     ref.current.innerHTML = "";
-//     ref.current.appendChild(Jazzicon(16, parseInt(account.slice(2, 10), 16)));
-//   }
-// }, [account]);
-
-// return <StyledIdenticon ref={ref as any} />;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="react-scripts" />
+// / <reference types="react-scripts" />
 
 declare module "@metamask/jazzicon" {
   export default function (diameter: number, seed: number): HTMLElement;
